d6347551bb90a6839586baf2832d2dcb855931f1
Vlad Baciu <baciuvladeusebiu@gmail.com>
2021-10-12 00:40:33 +0200

    Pass py dashboard through PEP8 verification

--- src/python_dashboard/project/ppg_dashboard.py
+++ src/python_dashboard/project/ppg_dashboard.py
@@ -1,27 +1,28 @@
 # -*- coding: utf-8 -*-
 from process_serial_frames import SerialFrame
 from PIL import Image
+from datetime import datetime
+
 import streamlit as st
 import plotly.graph_objects as go
 import plotly.express as px
 import pandas as pd
-from datetime import datetime
+
 
 class PPGDashboard:
     
     st.set_page_config(layout="wide")
  
-    row1_2, row2_2,row3_2 = st.columns((2,1,1))
+    row1_2, row2_2, row3_2 = st.columns((2, 1, 1))
 
-    col1, col2, col3 = st.columns((3,2,1))
-    _, r2_col1, r2_col2 = st.columns((2,2,4))
+    col1, col2, col3 = st.columns((3, 2, 1))
+    _, r2_col1, r2_col2 = st.columns((2, 2, 4))
     gauge_placeholder = col2.empty()
     chart_placeholder = col1.empty()
     spo_box_placeholder = col3.empty()
     image_placeholder = r2_col1.empty()
     image_text_placeholder = r2_col2.empty()
     
- 
     with row1_2:
         st.title("PPG signal")
     with row2_2:   
@@ -36,35 +37,31 @@ class PPGDashboard:
         fig.update_yaxes(showticklabels=False)
         self.chart_placeholder.write(fig)
        
-        
-    def bpm_gauge(self,heart_rate,previous_heart_rate):
+    def bpm_gauge(self, heart_rate, previous_heart_rate):
         fig = go.Figure(go.Indicator(
-            domain = {'x': [0, 1], 'y': [0, 1]},
-            value = heart_rate,
-            mode = "gauge+number+delta",
-            title = {'text': "BPM"},
-            delta = {'reference': previous_heart_rate},
-            gauge = {'axis': {'range': [50, 130]},
-                     'steps': [{'range': [50, 65], 'color': "lightgray"},
+            domain={'x': [0, 1], 'y': [0, 1]},
+            value=heart_rate,
+            mode="gauge+number+delta",
+            title={'text': "BPM"},
+            delta={'reference': previous_heart_rate},
+            gauge={'axis': {'range': [50, 130]},
+                   'steps': [{'range': [50, 65], 'color': "lightgray"},
                              {'range': [65, 85], 'color': "gray"},
                              {'range': [85, 130], 'color': "red"}],
-                     'threshold': {'line': {'color': "black", 'width': 4}, 'thickness': 0.75, 'value': 110}
-                     
-                     }))
+                   'threshold': {'line': {'color': "black", 'width': 4}, 'thickness': 0.75, 'value': 110}}))
         fig.update_layout(height=500, width=500)
         
         self.gauge_placeholder.write(fig)
 
     def spo_box(self, spo_value, color):
         box = go.Figure(go.Indicator(
-            domain = {'x': [0, 1], 'y': [0, 1]},
-            value = spo_value,
-            number = {'suffix': "%", 'font' : {'color' : color}},
-            mode = "number",
-            title = {'text': "SpO2"}))
+            domain={'x': [0, 1], 'y': [0, 1]},
+            value=spo_value,
+            number={'suffix': "%", 'font': {'color': color}},
+            mode="number",
+            title={'text': "SpO2"}))
         box.update_layout(height=300, width=300)
         
-       
         self.spo_box_placeholder.write(box)
             
     def show_image(self, tissue_detected, image):
@@ -85,14 +82,14 @@ def main():
     dashboard = PPGDashboard()
     previous_heart_rate = 0
     image_finger = Image.open('D:/VUB/ILFS/src/python_dashboard/project/finger_press_iconv.png')
-    ppg_record = pd.DataFrame(data=[],columns=['Sample','Amplitude'])
+    ppg_record = pd.DataFrame(data=[], columns=['Sample', 'Amplitude'])
     spo_color = "red"
     j = 0
     counter = 0
     plot_samples = 3
     chart_size = 200
     
-    dashboard.bpm_gauge(0,0)
+    dashboard.bpm_gauge(0, 0)
     dashboard.spo_box(0, spo_color)
     dashboard.ppg_chart(ppg_record)
 
@@ -105,11 +102,11 @@ def main():
             if(serialHandler.ir_buffer_updated):
                 for i in range(0, len(serialHandler.ir_buffer)-1):
                 
-                    ppg_record.loc[j,'Sample'] = counter
-                    ppg_record.loc[j,'Amplitude'] = serialHandler.ir_buffer[i]
+                    ppg_record.loc[j, 'Sample'] = counter
+                    ppg_record.loc[j, 'Amplitude'] = serialHandler.ir_buffer[i]
                     j = j + 1
-                    counter +=1
-                    if(counter % plot_samples ==0):
+                    counter += 1
+                    if(counter % plot_samples == 0):
                         dashboard.ppg_chart(ppg_record)
                     if(counter > chart_size):
                         if(counter % plot_samples == 0):
@@ -117,8 +114,7 @@ def main():
             serialHandler.ir_buffer.clear()
             serialHandler.red_buffer.clear()
             
-            
-            dashboard.bpm_gauge(serialHandler.heart_rate,previous_heart_rate)
+            dashboard.bpm_gauge(serialHandler.heart_rate, previous_heart_rate)
             
             if(serialHandler.spo2_value <= 92):
                 spo_color = "red"
@@ -127,8 +123,7 @@ def main():
             else:
                 spo_color = "green"
             
-            dashboard.spo_box(serialHandler.spo2_value,spo_color)
-            
+            dashboard.spo_box(serialHandler.spo2_value, spo_color)
         
             previous_heart_rate = serialHandler.heart_rate
             
--- src/python_dashboard/project/process_serial_frames.py
+++ src/python_dashboard/project/process_serial_frames.py
@@ -1,21 +1,24 @@
-
 import serial
 
 
 class SerialFrame:
-    heart_rate = 0;
-    spo2_value = 0;
-    tissue_detected = 0;
+    heart_rate = 0
+    spo2_value = 0
+    tissue_detected = 0
     ir_buffer = []
     red_buffer = []
     ir_buffer_updated = 0
     red_buffer_updated = 0
     heart_rate_spo_updated = 0
     error_frame_counter = 0
-    
+
     def __init__(self, serialCOM, baudRate):
-            self.serialHandler = serial.Serial(port=serialCOM, baudrate=baudRate, parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS)
-            if self.serialHandler.isOpen() == False:
+            self.serialHandler = serial.Serial(port=serialCOM, 
+                                               baudrate=baudRate, 
+                                               parity=serial.PARITY_NONE,
+                                               stopbits=serial.STOPBITS_ONE,
+                                               bytesize=serial.EIGHTBITS)
+            if self.serialHandler.isOpen() is False:
                 self.serialHandler.open()
                 
             self.frame = []
@@ -23,8 +26,8 @@ class SerialFrame:
     def checkFrameIntegrity(self):
 
         if(self.frameBytes[0] != 0xDA) or \
-          ((self.frameBytes[len(self.frameBytes) - 1] != 0xDC) and \
-          (self.frameBytes[len(self.frameBytes) - 2] != 0xEA)):
+          ((self.frameBytes[len(self.frameBytes) - 1] != 0xDC) and 
+           (self.frameBytes[len(self.frameBytes) - 2] != 0xEA)):
               
             return -1
         
@@ -34,7 +37,7 @@ class SerialFrame:
         
     def readFrame(self):
         self.frame = self.serialHandler.readline()
-        self.frameBytes =  bytes.fromhex(hex(int(self.frame.decode(),16))[2:])
+        self.frameBytes = bytes.fromhex(hex(int(self.frame.decode(), 16))[2:])
         
         return self.checkFrameIntegrity()
             
@@ -48,14 +51,14 @@ class SerialFrame:
             self.red_buffer_updated = 0
             
             if(self.frameBytes[1] == 0x83):
-                #process PARAM frame
-                self.tissue_detected = self.frameBytes[2];
+                # process PARAM frame
+                self.tissue_detected = self.frameBytes[2]
                 if(self.tissue_detected):
                     self.heart_rate = self.frameBytes[3]
                     self.spo2_value = self.frameBytes[4] + self.frameBytes[5]/100
                     self.heart_rate_spo_updated = 1
             elif(self.frameBytes[1] == 0x7C):
-                #process channel_data frame
+                # process channel_data frame
                 self.tissue_detected = self.frameBytes[2]
                 
                 if(self.tissue_detected):
@@ -64,14 +67,14 @@ class SerialFrame:
                     
                     counterBuff = 0
                     if(self.frameBytes[3] == 0x00):
-                        for x in range(0,len(tempBuff), 4):
+                        for x in range(0, len(tempBuff), 4):
                             tempBuff2.append(tempBuff[x] + (tempBuff[x+1] << 8) + (tempBuff[x+2] << 16) + (tempBuff[x+3] << 24))
                 
                         self.red_buffer_updated = 1
                         self.red_buffer = [max(tempBuff2)-x for x in tempBuff2]
                         
                     elif(self.frameBytes[3] == 0x02):
-                        for x in range(0,len(tempBuff), 4):
+                        for x in range(0, len(tempBuff), 4):
                             tempBuff2.append(tempBuff[x] + (tempBuff[x+1] << 8) + (tempBuff[x+2] << 16) + (tempBuff[x+3] << 24))
                             counterBuff += 1
                             
@@ -84,7 +87,7 @@ class SerialFrame:
                         self.red_buffer = 0
                         
             else:
-                #skip frame (DEBUG FRAME NOT SUPPORTED)
+                # skip frame (DEBUG FRAME NOT SUPPORTED)
                 self.error_frame_counter = self.error_frame_counter + 1
                 self.heart_rate_spo_updated = 0
                 self.ir_buffer_updated = 0
