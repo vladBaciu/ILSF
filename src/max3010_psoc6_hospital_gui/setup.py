8612851eb98a392d8e629a45c65e8ab1fb345493
Vlad Baciu <baciuvladeusebiu@gmail.com>
2021-10-28 17:51:29 +0200

    Delete unused files from PSOC gui app.
    Signed-off-by: Vlad Baciu <baciuvladeusebiu@gmail.com>

--- src/max3010_psoc6_hospital_gui/setup.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from distutils.core import setup
-import py2exe
-
-setup(options = {"py2exe":{   "excludes": ["_pytest","qtpy"] } },
-      console = ['python_interface.py'])
